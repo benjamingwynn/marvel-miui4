a463d4624640aa008a06921cb770aa38d99dea74
Michael <okoromichael354@gmail.com>
2012-07-31 12:51:13 -0500

    Update auto-build-script/MIUI-resources/reodex/reodex.sh

--- auto-build-script/MIUI-resources/reodex/reodex.sh
+++ auto-build-script/MIUI-resources/reodex/reodex.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "odex script by Tearsdontfalls"
+adb start-server
+adb wait-for-device
 adb push odex/ /data/local/tmp/odex
 adb shell chmod 755 /data/local/tmp/odex/*
 adb shell /data/local/tmp/odex/odex
 echo "finished"
+
+#### Modified by rezo609
